c37ad9bce6b68567a3137b2475e8ae6e0fb19123
Steven Wang <stephenwang0501@users.noreply.github.com>
2018-09-28 21:52:29 -0700

    Add app asset, change layout, change code

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,37 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button askButton;
+    private ImageView eightBall;
+    private int[] balls = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5};
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askButton = findViewById(R.id.askButton);
+        eightBall = findViewById(R.id.image_eightBall);
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int id = random.nextInt(5);
+                eightBall.setImageResource(balls[id]);
+            }
+        });
     }
 }
